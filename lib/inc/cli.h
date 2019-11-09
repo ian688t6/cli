054ff825b75e909bc99c1f78084aab1a18b3bc6e
ian688t6 <gagovski@163.com>
2019-11-09 20:24:04 +0800

    add cli_arg_s

--- lib/inc/cli.h
+++ lib/inc/cli.h
@@ -1,11 +1,31 @@
 #ifndef __CLI_H__
 #define __CLI_H__
+#include "list.h"
+
+#define CLI_ARG_BUFLEN		(256)
+#define CLI_ARG_NAMELEN		(32)
+
+typedef enum {
+	CLI_TYPE_UINT8,
+	CLI_TYPE_UINT16,
+	CLI_TYPE_UINT32,
+	CLI_TYPE_UINT64,
+	CLI_TYPE_STRING,
+	CLI_TYPE_BINARY,
+} cli_type_e;
 
 typedef struct {
 	const char	*pc_script;
 	int32_t 	i_argc;	
 	const char 	**ppc_argv;
 } cli_s;
+
+typedef struct {
+	cli_type_e	em_type;
+	char 		ac_arg_name[CLI_ARG_NAMELEN];
+	uint32_t	ui_size;
+	uint8_t		auc_arg_buf[CLI_ARG_BUFLEN];
+} cli_arg_s;
 
 extern int32_t cli_begin(cli_s *pst_cli);
 
