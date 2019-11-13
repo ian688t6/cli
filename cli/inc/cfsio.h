f1934f128f2e5ad973dd2963ebc0219e62efadf9
jia_yin <jia_yin@celfras.com>
2019-11-13 15:01:50 +0800

    add cfsio.c

---
+++ cli/inc/cfsio.h
@@ -0,0 +1,7 @@
+#ifndef __CFSIO_H__
+#define __CFSIO_H__
+
+extern void cfsio_init(void);
+
+#endif
+
---
+++ cli/src/cfsio.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cfsio.h"
+#include "cfs.h"
+#include "cli.h"
+
+static int32_t cfsio_get(uint16_t us_reg, uint8_t *puc_val, uint8_t uc_len)
+{
+	int32_t i_ret = 0;
+
+	return i_ret;
+}
+
+static int32_t cfsio_set(uint16_t us_reg, uint8_t *puc_val, uint8_t uc_len)
+{
+	int32_t i_ret = 0;
+
+	return i_ret;
+}
+
+void cfsio_init(void)
+{
+	return;
+}
+
+CFS_DEVICE_DEF(cfsio_set, cfsio_get)
